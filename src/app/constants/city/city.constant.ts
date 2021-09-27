685dae876153e1b597e59cc10d9ef6c73f3c398d
Alperen Talaslıoğlu <alperen.talaslioglu@gmail.com>
2021-09-27 20:19:48 +0300

    city mock data added

---
+++ src/app/constants/city/city.constant.ts
@@ -0,0 +1,10 @@
+import {City} from "../../models";
+
+export const CityList: City[] = [
+  {name: 'Amsterdam'},
+  {name: 'Berlin'},
+  {name: 'Barcelona'},
+  {name: 'Paris'},
+  {name: 'Rome'},
+  {name: 'Istanbul'}
+];
